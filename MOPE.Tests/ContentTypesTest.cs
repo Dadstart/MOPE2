b322cbfda2f247e7ceaa745cf59c015efe49d203
Andrew Bishop <andrew@bishop4.com>
2020-03-31 15:30:40 -0700

    Use Theory instead of Fact for some unit tests

--- MOPE.Tests/ContentTypesTest.cs
+++ MOPE.Tests/ContentTypesTest.cs
@@ -8,8 +8,10 @@ namespace MOPE.Tests
 {
 	public class ContentTypesTest
 	{
-		[Fact]
-		public void TestLoad()
+		[Theory]
+		[InlineData("application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml", "/word/document.xml")]
+		[InlineData("application/vnd.openxmlformats-officedocument.wordprocessingml.styles+xml", "/word/styles.xml")]
+		public void TestLoadOverrides(string contentType, string partUri)
 		{
 			ContentTypes ct;
 			var assembly = Assembly.GetExecutingAssembly();
@@ -18,7 +20,23 @@ namespace MOPE.Tests
 				ct = ContentTypes.Load(xmlFile);
 			}
 
-			Assert.Equal("image/jpeg", ct.Defaults["jpeg"]);
+			Assert.Equal(contentType, ct.Overrides[partUri]);
+		}
+
+		[Theory]
+		[InlineData("image/jpeg", "jpeg")]
+		[InlineData("application/xml", "xml")]
+		[InlineData("application/vnd.openxmlformats-package.relationships+xml", "rels")]
+		public void TestLoadDefaults(string contentType, string ext)
+		{
+			ContentTypes ct;
+			var assembly = Assembly.GetExecutingAssembly();
+			using (var xmlFile = assembly.GetManifestResourceStream("MOPE.Tests.Collateral.[Content_Types].xml"))
+			{
+				ct = ContentTypes.Load(xmlFile);
+			}
+
+			Assert.Equal(contentType, ct.Defaults[ext]);
 			Assert.Equal("application/vnd.openxmlformats-package.relationships+xml", ct.Defaults["rels"]);
 			Assert.Equal("application/xml", ct.Defaults["xml"]);
 
--- MOPE.Tests/RelationshipsTest.cs
+++ MOPE.Tests/RelationshipsTest.cs
@@ -1,4 +1,5 @@
 using B4.Mope.Packaging;
+using Moq;
 using System;
 using System.IO;
 using System.Reflection;
@@ -8,17 +9,21 @@ namespace MOPE.Tests
 {
 	public class RelationshipsTest
 	{
-		[Fact]
-		public void TestLoad()
+		[Theory]
+		[InlineData("rId1", "http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument", "word/document.xml")]
+		[InlineData("rId2", "http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties", "docProps/core.xml")]
+		[InlineData("rId3", "http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties", "docProps/app.xml")]
+		[InlineData("rId4", "http://schemas.openxmlformats.org/officeDocument/2006/relationships/custom-properties", "docProps/custom.xml")]
+		public void TestLoad(string id, string type, string target)
 		{
 			Relationships rels;
 			var assembly = Assembly.GetExecutingAssembly();
 			using (var xmlFile = assembly.GetManifestResourceStream("MOPE.Tests.Collateral..rels"))
 			{
-				rels = Relationships.Load(xmlFile);
+				rels = Relationships.Load(Mock.Of<Package>(), null, xmlFile);
 			}
 
-			AssertRel(rels["rId1"], "rId1", "http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument", "word/document.xml");
+			AssertRel(rels[id], id, type, target);
 		}
 
 		private void AssertRel(Relationship rel, string id, string type, string target)
@@ -26,6 +31,14 @@ namespace MOPE.Tests
 			Assert.Equal(id, rel.Id);
 			Assert.Equal(type, rel.Type);
 			Assert.Equal(target, rel.Target);
+		}
+
+		[Theory]
+		[InlineData("word/document.xml", "word/_rels/document.xml.rels")]
+		[InlineData(null, "_rels/.rels")]
+		public void TestGetRelationshipPartOwner(string source, string relsPart)
+		{
+			Assert.Equal(source, Relationships.GetRelationshipPartOwner(relsPart));
 		}
 	}
 }
