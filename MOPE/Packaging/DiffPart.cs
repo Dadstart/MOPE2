7456822b55f5387b7761cbb460abcc446511ce30
Andrew Bishop <andrew@bishop4.com>
2020-11-04 13:23:46 -0800

    Diff classes

---
+++ MOPE/Packaging/DiffPart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B4.Mope.Packaging
+{
+	public class DiffPart
+	{
+		public Part Left { get; }
+		public Part Right { get; }
+
+		public DiffPart(Part left, Part right)
+		{
+			// must have at least one part
+			if ((left == null) && (right == null))
+			{
+				throw new ArgumentNullException();
+			}
+
+			Left = left;
+			Right = right;
+		}
+	}
+}
